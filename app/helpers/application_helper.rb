9d4f17d9bc58140f6343faab7c331df8c30b838c
Obed Eugene <obed.eugene@gmail.com>
2013-06-16 14:21:51 -0400

    Edit Layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
-end
+
+  
+#  def title
+    base_title = "Social App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{title}"  
+    end  
+  end
+end   
