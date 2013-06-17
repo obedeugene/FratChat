16ae4ba9dee00079ef9d48d7c081ae8f9c3a31e4
Obed Eugene <obed.eugene@gmail.com>
2013-06-17 16:12:51 -0400

    Update Rails gem and update app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SocialApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  
+
+  get "static_pages/home"
+
+  get "static_pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
