e7ce708ab0f9a4e8821f44e34f0e991907bc73d0
Obed Eugene <obed.eugene@gmail.com>
2013-06-18 01:17:52 -0400

    Finish the Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   
 #  def title
-    base_title = "Social App"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{title}"  
-    end  
-  end
+#    base_title = "Social App"
+#    if @title.nil?
+#      base_title
+#    else
+#      "#{base_title} | #{title}"  
+#    end  
+#  end
 end   
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  
+ describe "Home Page" do
+
+  	it "should have the content 'Social App'" do
+  	   visit '/home'
+  	   page.should have_selector('h1', :text => 'Social App')
+  	end 
+
+    it "should have the right title" do
+       visit '/home'
+       page.should have_selector('title', :text => "Social App | Home")
+    end    
+ end
+
+ describe "Help Page" do
+ 
+  	it "should have the content 'Help'" do
+  	   visit '/help'
+       page.should have_selector('h1', :text => 'Help')
+  	end 
+
+    it "should have the right title" do
+       visit '/help'
+       page.should have_selector('title', :text => "Social App | Help")
+    end    
+  end
+
+ describe "About Page" do
+
+  	it "should have the content 'About'" do
+  	   visit '/about'
+       page.should have_selector('h1', :text => 'About Us')
+  	end 	
+
+    it "should have the right title" do
+       visit '/about'
+       page.should have_selector('title', :text => "Social App | About Us")
+    end    
+  end
+
+ describe "Contact" do
+
+    it "should have the first h1 'Contact'" do
+       visit '/contact'
+       page.should have_selector('h1', :text => 'Contact')
+    end   
+
+    it "should have the right title" do
+       visit '/contact'
+       page.should have_selector('title', :text => "Social App | Contact")
+    end    
+  end
+end
+
