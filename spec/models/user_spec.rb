cc8460615a95e5738cbb8f7642925a070766211c
Obed Eugene <obed.eugene@gmail.com>
2013-06-21 19:12:37 -0400

    Edit model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,9 +14,9 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should respond_to(:remember_token) }
-  it { should respond_to(:admin) }
-  it { should respond_to(:authenticate) }
+  # it { should respond_to(:remember_token) }
+  # it { should respond_to(:admin) }
+  # it { should respond_to(:authenticate) }
   # it { should respond_to(:microposts) }
   # it { should respond_to(:feed) }
   # it { should respond_to(:relationships) }
@@ -27,25 +27,25 @@ describe User do
   # it { should respond_to(:follow!) }
   # it { should respond_to(:unfollow!) }
 
-  it { should be_valid }
-  it { should_not be_admin }
+  # it { should be_valid }
+  # it { should_not be_admin }
 
-  describe "accessible attributes" do
-    it "should not allow access to admin" do
-      expect do
-        User.new(admin: true)
-      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
-    end
-  end
+  # describe "accessible attributes" do
+  #   it "should not allow access to admin" do
+  #     expect do
+  #       User.new(admin: true)
+  #     end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+  #   end
+  # end
 
-  describe "with admin attribute set to 'true'" do
-    before do
-      @user.save!
-      @user.toggle!(:admin)
-    end
+  # describe "with admin attribute set to 'true'" do
+  #   before do
+  #     @user.save!
+  #     @user.toggle!(:admin)
+  #   end
 
-    it { should be_admin }
-  end
+  #   it { should be_admin }
+  # end
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -117,10 +117,11 @@ describe User do
     it { should_not be_valid }
   end
 
-  desc ribe "with a password that's too short" do
+  describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+
 
   describe "return value of authenticate method" do
     before { @user.save }
